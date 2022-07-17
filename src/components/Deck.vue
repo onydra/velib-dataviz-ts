06faaece44181e109ffad127ce4bf226fd3e43c8
Dorian Conq <dorian.conq@gmail.com>
2022-07-17 17:03:56 +0200

    feat: Added mapbox map centered on Paris

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -6,8 +6,37 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import mapboxgl from 'mapbox-gl'
 
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+</script>
+<script lang="ts">
+
+const INITIAL_VIEW_STATE = {
+    latitude: 48.8566,
+    longitude: 2.3522,
+    zoom: 12,
+    minZoom: 10,
+    // maxZoom: 15,
+    pitch: 40.5,
+    bearing: 30
+}
+
+export default {
+    name: 'Deck',
+    mounted() {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/nayrrod/ck5o57cn71g3z1ioai8gebzj7',
+            // Note: deck.gl will be in charge of interaction and event handling
+            interactive: false,
+            center: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude],
+            zoom: INITIAL_VIEW_STATE.zoom,
+            bearing: INITIAL_VIEW_STATE.bearing,
+            pitch: INITIAL_VIEW_STATE.pitch
+        })
+    }
+}
 </script>
 
 <style scoped lang="postcss">
